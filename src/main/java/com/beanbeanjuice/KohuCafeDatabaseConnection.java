c2c9d8f447164eff3063aa3fb1925095c6f9f860
beanbeanjuice <beanbeanjuice@outlook.com>
2022-08-03 03:52:59 -0400

    Added Starting Code

---
+++ src/main/java/com/beanbeanjuice/KohuCafeDatabaseConnection.java
@@ -0,0 +1,11 @@
+package com.beanbeanjuice;
+
+import org.jetbrains.annotations.NotNull;
+
+public class KohuCafeDatabaseConnection {
+    
+    public KohuCafeDatabaseConnection(@NotNull String username) {
+        System.out.println(username);
+    }
+
+}
