4aa64269278ddc1d203ff0d20a00560ad27a90ff
beanbeanjuice <beanbeanjuice@outlook.com>
2022-08-05 01:21:20 -0400

    Testing

--- src/test/java/TestSQLConnection.java
+++ src/test/java/TestSQLConnection.java
@@ -8,16 +8,17 @@ public class TestSQLConnection {
 
     private static final Dotenv DOT_ENV = Dotenv.load();
     private static final String MYSQL_URL = DOT_ENV.get("MYSQL_URL");
-    private static final Integer MYSQL_PORT = Integer.parseInt(DOT_ENV.get("MYSQL_PORT"));
-    private static final String MYSQL_SCHEMA = DOT_ENV.get("MYSQL_SCHEMA");
-    private static final String MYSQL_USERNAME = DOT_ENV.get("MYSQL_USERNAME");
-    private static final String MYSQL_PASSWORD = DOT_ENV.get("MYSQL_PASSWORD");
+//    private static final Integer MYSQL_PORT = Integer.parseInt(DOT_ENV.get("MYSQL_PORT"));
+//    private static final String MYSQL_SCHEMA = DOT_ENV.get("MYSQL_SCHEMA");
+//    private static final String MYSQL_USERNAME = DOT_ENV.get("MYSQL_USERNAME");
+//    private static final String MYSQL_PASSWORD = DOT_ENV.get("MYSQL_PASSWORD");
 
     @Test
     @DisplayName("Test Connection")
     public void testConnection() {
-        SQLConnection connection = new SQLConnection(MYSQL_URL, MYSQL_PORT, MYSQL_SCHEMA, MYSQL_USERNAME, MYSQL_PASSWORD);
-        Assertions.assertTrue(connection.start());
+        System.out.println("URL: " + MYSQL_URL);
+//        SQLConnection connection = new SQLConnection(MYSQL_URL, MYSQL_PORT, MYSQL_SCHEMA, MYSQL_USERNAME, MYSQL_PASSWORD);
+//        Assertions.assertTrue(connection.start());
     }
 
 }
